2dc7026c23d2390fe4ad23691e1868ca269e2a03
Kirill Pavlov <pavlov99@yandex.ru>
2016-09-27 18:26:55 +0800

    Init mdb-export-all

---
+++ mdb-export-all.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+command -v mdb-tables >/dev/null 2>&1 || {
+    echo >&2 "I require mdb-tables but it's not installed. Aborting.";
+    exit 1;
+}
+
+command -v mdb-export >/dev/null 2>&1 || {
+    echo >&2 "I require mdb-export but it's not installed. Aborting.";
+    exit 1;
+}
+
+fullfilename=$1
+filename=$(basename "$fullfilename")
+dbname="${filename%.*}"
+
+mkdir $dbname
+
+for table in $(mdb-tables $fullfilename); do
+    echo "Export table "$table
+    mdb-export $fullfilename $table > $dbname/$table.csv
+done
